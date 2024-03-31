243cf66f9f92155ba1ee1a64c83f311028bee099
shiki <kishintorii@gmail.com>
2024-03-31 15:28:19 +0900

    🎉 start

---
+++ src/lib/Block.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let el: HTMLElement;
+
+  onMount(() => {
+    // ドラッグアンドドロップのロジックをここに実装します。
+  });
+</script>
+
+<div bind:this={el}>
+  <slot></slot>
+</div>
